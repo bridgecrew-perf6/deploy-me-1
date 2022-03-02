58ee20ada1d536da653cc4fdb0579bb7f303b524
plotinusspascual <plotinusspascual@gmail.com>
2022-03-02 12:56:00 -0800

    Updates

---
+++ precache-manifest.4c2bf5848a646b0391ca34fa754513b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "543b416949513929d1d6",
+    "url": "/deploy-me/static/css/main.e8ecec78.chunk.css"
+  },
+  {
+    "revision": "543b416949513929d1d6",
+    "url": "/deploy-me/static/js/main.340b3c81.chunk.js"
+  },
+  {
+    "revision": "b1ca2f576a0c5c9f1249",
+    "url": "/deploy-me/static/js/runtime~main.a7dea211.js"
+  },
+  {
+    "revision": "0f195abedd5f7ef66ebf",
+    "url": "/deploy-me/static/js/2.eedbfbed.chunk.js"
+  },
+  {
+    "revision": "fa0614dbe325095172dbe3e1207e41ef",
+    "url": "/deploy-me/index.html"
+  }
+];
